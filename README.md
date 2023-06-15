5c62995297b41781dd9d307e2b6c88347a47adc0
chen <chen.hsieh@uga.edu>
2023-06-15 16:56:23 -0400

    realize the sorting

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ using streamlit to aid the screening process of the MMP of TYRA
 ```
 streamlit run app.py
 ```
+
+## the logic of the app
+
+1. mentors input the verificaiton code received from the email
+
+2. the app fetch the subset of the processed mentees' responses from the google sheet
+
+3. mentors can view the responses, compare, and then report to the google form
--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import streamlit.components.v1 as components
 
-st.title('MMP Screening App Prototype')
+st.set_page_config(
+    page_title="MMP Screening App",
+    page_icon="⛵️",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': """
+# Find more about TYRA
+https://linktr.ee/projecttyra
+"""
+    }
+)
+st.title('MMP Screening App')
+st.sidebar.markdown('''
+# Sections
+- [Know your mentee better](#know-your-mentee-better)
+- [Finalize your decision](#finalize-your-decision)
+''', unsafe_allow_html=True)
 
 sheet_url = st.secrets["sheet_url"]
 csv_export_url = sheet_url.replace('/edit#gid=', '/export?format=csv&gid=')
 
+mentors_sheet_url = st.secrets["mentors_sheet_url"]
+mentors_csv_export_url = mentors_sheet_url.replace(
+    '/edit#gid=', '/export?format=csv&gid=')
+mentors = pd.read_csv(mentors_csv_export_url)
+
+
 @st.cache_data
-def load_data(mentor_name):
+def load_mentee_data(mentor_name):
     data = pd.read_csv(csv_export_url)
     # lowercase = lambda x: str(x).lower()
     # data.rename(lowercase, axis='columns', inplace=True)
@@ -18,47 +42,19 @@ def load_data(mentor_name):
         (data["希望配對的導師（第二志願）.1"] == mentor_name) |
         (data["希望配對的導師（第三志願）.1"] == mentor_name) |
         (data["希望配對的導師（第四志願）.1"] == mentor_name) |
-        (data["希望配對的導師（第五志願）.1"] == mentor_name) 
-        #TODO: reduce the columns to be shown
+        (data["希望配對的導師（第五志願）.1"] == mentor_name)
     ]
     return data
+
 
 @st.cache_data
 def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
 
-mentor_name = st.text_input('Please input your "mentor ID" from the email', 'C10 謝明修') #TODO: modify the default value
-data_load_state = st.text('Loading data...')
-data = load_data(mentor_name) # example mentor: "C10 謝明修", "B39 張櫂杬"
-data_load_state.text("Mentee data retrieved!")
-st.text(f'You got these candidate mentees: {data["中文姓名"].values}')
-
-"You can click the checkbox below to see all the raw data of the candidate mentees."
-if st.checkbox('Show raw data of candidate mentees'):
-    st.subheader('Raw data')
-    st.write(data)
-    csv = convert_df(data)
-    st.download_button(
-        label="Download data as CSV",
-        data=csv,
-        file_name=f'{mentor_name}_candidate_mentee.csv',
-        mime='text/csv',
-    )
-
-st.subheader('Understand your mentees better!')
-
-st.radio(
-    "Choose your mentee data display mode",
-    ["Single Mentee Info", "Multiple Mentee Info"],
-    key="viewing_mode",
-    horizontal=True,
-)
 
 display_columns = [
-    "電子郵件地址",
     "中文姓名",
-    "其餘聯絡方式 (非必填)",
     "最高學歷",
     "學士就讀/畢業學校",
     "學士就讀/畢業系所",
@@ -88,33 +84,103 @@ display_columns = [
     "任何想補充給導師知道的（選填）",
     "目前是否已開始聯繫請求推薦信。",
     "是否已經參加標準化入學考試？",
+    "電子郵件地址",
+    "其餘聯絡方式 (非必填)",
+    "志願序",
 ]
 
-if st.session_state.viewing_mode == "Single Mentee Info":
-    st.subheader("Single Mentee Info")
-    mentee_name = st.selectbox(
-        'You can select one mentee to see his/her profile.',
-        data["中文姓名"].values)
+# TODO: modify the default value and change to the verification code system
+mentor_name = st.text_input(
+    'Please input your "verification code" from the email', 'C10 謝明修')
+# example mentor: "C10 謝明修", "B39 張櫂杬"
+mentee_response = load_mentee_data(mentor_name)
+# data_load_state.text("Mentee's response retrieved!")
+candidate_mentee_number = mentee_response.shape[0]
+if candidate_mentee_number == 0:
+    st.warning(
+        f"Oops! No mentee is interested in you! Please check your name again.")
+    st.stop()
+elif candidate_mentee_number == 1:
 
-    st.dataframe(
-        data.loc[
-            data["中文姓名"] == mentee_name,display_columns
-                ].set_index("中文姓名").transpose(),
-        use_container_width=True
-        )
-elif st.session_state.viewing_mode == "Multiple Mentee Info":
+    st.success(
+        f"Great! {mentee_response['中文姓名'].values[0]} is interested in you!")
+else:
+
+    st.success(
+        f'You are popular! Here are the mentees who are interested in you: {"、".join(mentee_response["中文姓名"].values)}')
+
+# extract the ranking of the mentor from the mentee response
+mentee_response.loc[mentee_response["希望配對的導師（第五志願）.1"]
+                    == mentor_name, "志願序"] = "5"
+mentee_response.loc[mentee_response["希望配對的導師（第四志願）.1"]
+                    == mentor_name, "志願序"] = "4"
+mentee_response.loc[mentee_response["希望配對的導師（第三志願）.1"]
+                    == mentor_name, "志願序"] = "3"
+mentee_response.loc[mentee_response["希望配對的導師（第二志願）.1"]
+                    == mentor_name, "志願序"] = "2"
+mentee_response.loc[mentee_response["希望配對的導師（第一志願）"]
+                    == mentor_name, "志願序"] = "1"
+
+# filter the columns to be shown
+mentee_response = mentee_response[display_columns].sort_values(by="志願序")
+
+if st.checkbox('Show and download raw mentee response'):
+    st.write(mentee_response.set_index("中文姓名"))
+    csv = convert_df(mentee_response.set_index("中文姓名"))
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name=f'{mentor_name}_candidate_mentee.csv',
+        mime='text/csv',
+    )
+
+st.header('Know your mentee better!')
+
+if candidate_mentee_number == 1:
+    viewing_mode = "Single Mentee Info"
+elif candidate_mentee_number > 1:
+    viewing_mode = st.radio(
+        "Choose your mentee data display mode",
+        ["Single Mentee Info", "Multiple Mentee Info"],
+        # key="viewing_mode",
+        horizontal=True,
+    )
+
+
+if viewing_mode == "Single Mentee Info":
+    if candidate_mentee_number == 1:
+        mentee_name = mentee_response["中文姓名"].values[0]
+    if candidate_mentee_number > 1:
+        mentee_name = st.selectbox(
+            'You can select one of the candidate mentees to see their profile.',
+            mentee_response["中文姓名"].values)
+
+    for column in display_columns:
+        if (pd.isna(mentee_response.loc[mentee_response["中文姓名"]
+                                        == mentee_name, column]).any()):
+            continue
+        st.subheader(column)
+        st.write(mentee_response.loc[mentee_response["中文姓名"]
+                                     == mentee_name, column].values[0])
+elif viewing_mode == "Multiple Mentee Info":
     st.subheader("Multiple Mentee Info")
     options = st.multiselect(
         'You can select up to 3 mentees to see compare their profiles.',
-        data["中文姓名"].values,
+        mentee_response["中文姓名"].values,
         max_selections=3)
 
     st.dataframe(
-        data.loc[
-            data["中文姓名"].isin(options),display_columns].set_index("中文姓名").transpose(),
+        mentee_response.loc[
+            mentee_response["中文姓名"].isin(options), display_columns].set_index("中文姓名").transpose(),
+        height=500,
         use_container_width=True
-        )
+    )
+st.divider()
+st.markdown("[go back to top](#know-your-mentee-better)")
+st.header("Finalize your decision")
 
-st.subheader("Please use the following Google form to let us know your decision on the mentees you want to mentor.")
+f"""Please use the following Google form to let us know your decision on the mentees you want to mentor. We will let you know the final result after the matching process is done. 
 
-"google form link (WIP)" #TODO: add the link to the google form
+If the following part is not shown, please use this link: https://forms.gle/KbvU9C6TPZZ3h3wV6"""
+components.iframe("https://forms.gle/KbvU9C6TPZZ3h3wV6",
+                  width=None, height=1069, scrolling=True)

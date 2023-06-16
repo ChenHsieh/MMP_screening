a755b7d57a341db371c79c30cc124be689e9d910
chen <chen.hsieh@uga.edu>
2023-06-16 09:10:52 -0400

    modify the format of single mentee info mode

--- app.py
+++ app.py
@@ -16,16 +16,17 @@ https://linktr.ee/projecttyra
     }
 )
 st.title('MMP Screening App')
-st.sidebar.markdown('''
-# Sections
-- [Know your mentee better](#know-your-mentee-better)
-- [Finalize your decision](#finalize-your-decision)
-''', unsafe_allow_html=True)
+# st.sidebar.markdown('''
+# # Sections
+# - [Know your mentee better](#know-your-mentee-better)
+# - [Finalize your decision](#finalize-your-decision)
+# ''', unsafe_allow_html=True)
+mentee_response_sheet_url = "https://docs.google.com/spreadsheets/d/16kBkV86fkBqIoxedxIoY_rlJfRXlRKOT831fvzUv6Q4/edit#gid=1994387913"
+mentors_sheet_url = "https://docs.google.com/spreadsheets/d/1n4JdRzUb_J97Zh6UMDDwzao2GSYfgMhfj3NKn_uLHsI/edit#gid=164489593"
 
-sheet_url = st.secrets["sheet_url"]
-csv_export_url = sheet_url.replace('/edit#gid=', '/export?format=csv&gid=')
 
-mentors_sheet_url = st.secrets["mentors_sheet_url"]
+mentee_response_csv_export_url = mentee_response_sheet_url.replace(
+    '/edit#gid=', '/export?format=csv&gid=')
 mentors_csv_export_url = mentors_sheet_url.replace(
     '/edit#gid=', '/export?format=csv&gid=')
 mentors = pd.read_csv(mentors_csv_export_url)
@@ -33,7 +34,7 @@ mentors = pd.read_csv(mentors_csv_export_url)
 
 @st.cache_data
 def load_mentee_data(mentor_name):
-    data = pd.read_csv(csv_export_url)
+    data = pd.read_csv(mentee_response_csv_export_url)
     # lowercase = lambda x: str(x).lower()
     # data.rename(lowercase, axis='columns', inplace=True)
     # data["時間戳記"] = pd.to_datetime(data["時間戳記"])
@@ -91,14 +92,14 @@ display_columns = [
 
 # TODO: modify the default value and change to the verification code system
 mentor_name = st.text_input(
-    'Please input your "verification code" from the email', 'C10 謝明修')
+    'Please input your "verification code" from the email', '')
 # example mentor: "C10 謝明修", "B39 張櫂杬"
 mentee_response = load_mentee_data(mentor_name)
 # data_load_state.text("Mentee's response retrieved!")
 candidate_mentee_number = mentee_response.shape[0]
 if candidate_mentee_number == 0:
     st.warning(
-        f"Oops! No mentee is interested in you! Please check your name again.")
+        f"Oops! We cannot find any results for the current input. Please check your verification code.")
     st.stop()
 elif candidate_mentee_number == 1:
 
@@ -133,7 +134,7 @@ if st.checkbox('Show and download raw mentee response'):
         file_name=f'{mentor_name}_candidate_mentee.csv',
         mime='text/csv',
     )
-
+st.divider()
 st.header('Know your mentee better!')
 
 if candidate_mentee_number == 1:
@@ -154,14 +155,91 @@ if viewing_mode == "Single Mentee Info":
         mentee_name = st.selectbox(
             'You can select one of the candidate mentees to see their profile.',
             mentee_response["中文姓名"].values)
+    current_mentee = mentee_response.loc[mentee_response["中文姓名"]
+                                         == mentee_name]
+    # TODO optimize the single display mode
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric(f"選擇您為第 {current_mentee['志願序'].values[0]} 志願", current_mentee["中文姓名"].values[0])
+        st.write(
+            f"申請 {current_mentee['申請年份'].values[0]} {current_mentee['欲申請學位'].values[0]}")
+        st.subheader("學歷資料")
+        # current_mentee['最高學歷'].values[0]
+        for degree in ["學士", "碩士"]:
+            if (pd.isna(current_mentee[f'{degree}就讀/畢業學校'].values[0])):
+                continue
+            st.write(
+                f"{current_mentee[f'{degree}就讀/畢業學校'].values[0]} {current_mentee[f'{degree}就讀/畢業系所'].values[0]} {degree}")
 
-    for column in display_columns:
-        if (pd.isna(mentee_response.loc[mentee_response["中文姓名"]
-                                        == mentee_name, column]).any()):
+        st.subheader("申請目標")
+        goal_columns = [
+            "主要申請系所所在國家",
+            "欲申請學校",
+            "欲申請系所/program",
+            "相關的選擇學校、系所的理由",
+        ]
+        for column in goal_columns:
+            if (pd.isna(current_mentee[column]).any()):
+                continue
+            st.caption(column)
+            st.write(current_mentee[column].values[0])
+
+        st.subheader("任何想對未來導師說的話")
+        current_mentee["任何想對未來導師說的話"].values[0]
+
+    with col2:
+        st.subheader("基本資料")
+        st.caption(f"廣義研究領域")
+        current_mentee['廣義研究領域'].values[0]
+        st.caption(f"專業領域")
+        current_mentee['專業領域'].values[0]
+
+        st.subheader("目前申請準備進度")
+        progress_columns = ["欲尋求之協助/建議(至多三個)",
+                            "請問您目前準備進度為何？是否已經完成選校？",
+                            "提供簡歷與相關資料",
+                            "是否已經參加留學國的語文程度測驗？",
+                            "目前申請文件的準備進度？",
+                            "目前是否已開始聯繫請求推薦信。",
+                            "是否已經參加標準化入學考試？", ]
+        for column in progress_columns:
+            if (pd.isna(current_mentee[column]).any()):
+                continue
+            st.caption(column)
+            st.write(current_mentee[column].values[0])
+
+    st.subheader("選填背景資訊")
+    background_columns = [
+        "是否為家族中第一代高等教育子女（選填）",
+        "國際活動經驗（選填）",
+        "學術領域外相關特殊專長、經驗或成就（選填）",
+        "家人、伴侶等狀態（選填）",
+        "目前的生涯規劃，或主要申請目的與動機（選填）",
+        "您認為可能會影響申請過程或結果的身份、背景、經歷，而您希望導師知道的（選填）",
+        "任何想補充給導師知道的（選填）",
+        "任何公開資訊 （選填）", ]
+    for column in background_columns:
+        if (pd.isna(current_mentee[column]).any()):
             continue
-        st.subheader(column)
-        st.write(mentee_response.loc[mentee_response["中文姓名"]
-                                     == mentee_name, column].values[0])
+        st.caption(column)
+        st.write(current_mentee[column].values[0])
+
+    st.subheader("聯絡方式")
+    contact_columns = [
+    "電子郵件地址",
+    "其餘聯絡方式 (非必填)",
+    ]
+    for column in contact_columns:
+        if (pd.isna(current_mentee[column]).any()):
+            continue
+        st.caption(column)
+        st.write(current_mentee[column].values[0])
+
+    # for column in display_columns:
+    #     if (pd.isna(current_mentee[column]).any()):
+    #         continue
+    #     st.subheader(column)
+    #     st.write(current_mentee[column].values[0])
 elif viewing_mode == "Multiple Mentee Info":
     st.subheader("Multiple Mentee Info")
     options = st.multiselect(
@@ -172,7 +250,7 @@ elif viewing_mode == "Multiple Mentee Info":
     st.dataframe(
         mentee_response.loc[
             mentee_response["中文姓名"].isin(options), display_columns].set_index("中文姓名").transpose(),
-        height=500,
+        height=696,
         use_container_width=True
     )
 st.divider()

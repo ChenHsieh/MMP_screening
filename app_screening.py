53f00ea45ae11018a3b322c2a340d784f8ae4088
chen <chen.hsieh@uga.edu>
2024-07-05 12:02:12 -0400

    config

--- app_screening.py
+++ app_screening.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import streamlit.components.v1 as components
+import toml
+import os
 
 st.set_page_config(
     page_title="MMP Screening App",
@@ -20,13 +22,21 @@ https://github.com/ChenHsieh/MMP_screening
 )
 st.title('project Tyra - Mentor Dashboard')
 
-mentee_response_sheet_url = st.secrets["sheet_url"]
-mentors_sheet_url = st.secrets["mentors_sheet_url"]
+# Determine environment (local or cloud)
+environment = os.getenv("ENVIRONMENT", "local")
 
+# Set URLs based on environment
+if environment == "local":
+    # Load configuration
+    config = toml.load("config.toml")
+    mentee_response_sheet_url = config["mentee_response_sheet_url"]
+    mentors_sheet_url = config["mentors_sheet_url"]
+else:
+    mentee_response_sheet_url = st.secrets["sheet_url"]
+    mentors_sheet_url = st.secrets["mentors_sheet_url"]
 
 mentee_response_csv_export_url = mentee_response_sheet_url.replace(
     '/edit#gid=', '/export?format=csv&gid=')
-
 
 mentors_csv_export_url = mentors_sheet_url.replace(
     '/edit#gid=', '/export?format=csv&gid=')

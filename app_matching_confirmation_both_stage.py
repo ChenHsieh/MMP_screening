172cff115c8973a2ed3d75cb74a0c5f783fa2077
chen <chen.hsieh@uga.edu>
2024-08-27 17:09:22 -0400

    title mod

--- app_matching_confirmation_both_stage.py
+++ app_matching_confirmation_both_stage.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(
-    page_title="MMP Screening App",
+    page_title="Mentor Dashboard",
     page_icon="⛵️",
     layout="wide",
     initial_sidebar_state="expanded",
